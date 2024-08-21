a24b6cd6493a993a3c390d0beb97bb6089395a0d
fekadusisay21 <fekadusisay07@gmail.com>
2024-08-21 15:03:14 +0300

    last commit

--- pages/iphone/index.vue
+++ pages/iphone/index.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="grid grid-cols-4 gap-4 mt-10">
     <nuxt-link
-      to="/iphone/iphone-12"
+      v-for="iphone in iphones"
+      :key="iphone"
+      :to="`/iphone/${iphone}`"
       class="shadow-lg broder rounded-lg text-center"
     >
-      <h1>Iphone 12</h1>
+      <h1>{{ iphone }}</h1>
       <div class="flex justify-center">
-        <img width="200" src="/images/iphone12.webp" alt="" />
-      </div>
-    </nuxt-link>
-
-    <nuxt-link
-      to="/iphone/iphone-12-pro"
-      class="shadow-lg broder rounded-lg text-center"
-    >
-      <h1>Iphone 12 Pro</h1>
-      <div class="flex justify-center">
-        <img width="200" src="/images/iphone12pro.webp" alt="" />
+        <img
+          width="200"
+          :src="`/images/${iphone.replaceAll('-', '')}.webp`"
+          alt=""
+        />
       </div>
     </nuxt-link>
   </div>
@@ -26,4 +22,7 @@
 useHead({
   title: "Nuxt3 - Iphones",
 });
+
+const { data } = useFetch("/api/iphones");
+const iphones = data.value;
 </script>
