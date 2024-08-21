5bf348c9d74ea0867afd57ab10a5857f6ec04694
fekadusisay21 <fekadusisay07@gmail.com>
2024-08-21 03:04:12 +0300

    lazy components

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <HomeWelcome />
-    <Counter />
+    <NuxtPage />
+    <a href="#" @click="view('home')">Home</a>
+    <a href="#" @click="view('counter')">Counter</a>
+
+    <LazyHomeWelcome v-if="page === 'home'" />
+    <LazyCounter v-if="page === 'counter'" />
   </div>
 </template>
+
+<script setup>
+const page = ref("home");
+
+const view = (name) => {
+  page.value = name;
+};
+</script>
